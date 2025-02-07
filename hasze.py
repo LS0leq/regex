9dfc227e044b17c40c610146f626f316a6e5d220
LS0leq <ALEWASO51@edu.gdansk.pl>
2025-02-07 08:34:46 +0100

    Update regex.py

--- hasze.py
+++ hasze.py
@@ -3,7 +3,7 @@ import re
 with open("hasze.txt", "r", encoding="utf-8") as file:
     content = file.read()
 
-regex = "\#[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+"
+regex = "\#[a-zA-Z0-9_ąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+"
 
 words = re.findall(regex, content)
 print("Wczytane hasztagi:")
